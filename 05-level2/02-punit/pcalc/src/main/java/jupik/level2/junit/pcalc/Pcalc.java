4119a9db9c63433c552e73bc37a007559619283a
Boris Stankevich <microsoft-wanted@yandex.ru>
2025-06-29 22:27:57 +0300

    [05-LEVEL2] extend computation with strings
    Extend calculation to accept string representations of parameters.
    
    Signed-off-by: Boris Stankevich <microsoft-wanted@yandex.ru>

--- 05-level2/02-punit/pcalc/src/main/java/jupik/level2/junit/pcalc/Pcalc.java
+++ 05-level2/02-punit/pcalc/src/main/java/jupik/level2/junit/pcalc/Pcalc.java
@@ -4,4 +4,28 @@ import jupik.level2.junit.ucalc.Ucalc;
 
 public class Pcalc extends Ucalc
 {
+	public static double add(String a, String b)
+	{
+		return add(Double.parseDouble(a), Double.parseDouble(b));
+	}
+
+	public static double sub(String a, String b)
+	{
+		return sub(Double.parseDouble(a), Double.parseDouble(b));
+	}
+
+	public static double mul(String a, String b)
+	{
+		return mul(Double.parseDouble(a), Double.parseDouble(b));
+	}
+
+	public static double div(String a, String b)
+	{
+		return div(Double.parseDouble(a), Double.parseDouble(b));
+	}
+
+	public static double pow2(String a)
+	{
+		return pow2(Double.parseDouble(a));
+	}
 }
--- 05-level2/02-punit/pcalc/src/test/java/jupik/level2/punit/pcalc/PcalcTest.java
+++ 05-level2/02-punit/pcalc/src/test/java/jupik/level2/punit/pcalc/PcalcTest.java
@@ -2,6 +2,7 @@ package jupik.level2.punit.pcalc;
 
 import java.util.stream.Stream;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.api.Assertions;
@@ -26,6 +27,26 @@ public class PcalcTest
 		Assertions.assertEquals(a + b, Pcalc.add(a, b), EPS);
 	}
 
+	@DisplayName("Test addition of strings ")
+	@ParameterizedTest(name = "\"{0}\" and \"{1}\"")
+	@MethodSource("getAdditionOperands")
+	void testStringAddition(double a, double b)
+	{
+		Assertions.assertEquals(
+			a + b, Pcalc.add(Double.toString(a), Double.toString(b)), EPS
+		);
+	}
+
+	@DisplayName("Test a format exception on addition of strings ")
+	@ParameterizedTest(name = "\"{0}\" and \"{1}\"")
+	@CsvSource({"a,1.1", "2,a"})
+	void testStringAdditionFormatException(String a, String b)
+	{
+		Assertions.assertThrows(
+			NumberFormatException.class, () -> Pcalc.add(a, b)
+		);
+	}
+
 	static Stream<Arguments> getSubtractionOperands() {
 		return Stream.of(
 			Arguments.of(1, 1.1),
@@ -41,6 +62,26 @@ public class PcalcTest
 		Assertions.assertEquals(a - b, Pcalc.sub(a, b), EPS);
 	}
 
+	@DisplayName("Test subtraction of strings ")
+	@ParameterizedTest(name = "\"{1}\" from \"{0}\"")
+	@MethodSource("getSubtractionOperands")
+	void testStringSubtraction(double a, double b)
+	{
+		Assertions.assertEquals(
+			a - b, Pcalc.sub(Double.toString(a), Double.toString(b)), EPS
+		);
+	}
+
+	@DisplayName("Test a format exception on subtraction of strings ")
+	@ParameterizedTest(name = "\"{1}\" from \"{0}\"")
+	@CsvSource({"a,1.1", "2,a"})
+	void testStringSubtractionFormatException(String a, String b)
+	{
+		Assertions.assertThrows(
+			NumberFormatException.class, () -> Pcalc.sub(a, b)
+		);
+	}
+
 	static Stream<Arguments> getMultiplicationOperands() {
 		return Stream.of(
 			Arguments.of(1, 1.1),
@@ -54,6 +95,26 @@ public class PcalcTest
 	void testMultiplication(double a, double b)
 	{
 		Assertions.assertEquals(a * b, Pcalc.mul(a, b), EPS);
+	}
+
+	@DisplayName("Test multiplication of strings ")
+	@ParameterizedTest(name = "\"{0}\" and \"{1}\"")
+	@MethodSource("getMultiplicationOperands")
+	void testStringMultiplication(double a, double b)
+	{
+		Assertions.assertEquals(
+			a * b, Pcalc.mul(Double.toString(a), Double.toString(b)), EPS
+		);
+	}
+
+	@DisplayName("Test a format exception on multiplication of strings ")
+	@ParameterizedTest(name = "\"{0}\" and \"{1}\"")
+	@CsvSource({"a,1.1", "2,a"})
+	void testStringMultiplicationFormatException(String a, String b)
+	{
+		Assertions.assertThrows(
+			NumberFormatException.class, () -> Pcalc.mul(a, b)
+		);
 	}
 
 	static Stream<Arguments> getDivisionOperands() {
@@ -72,6 +133,26 @@ public class PcalcTest
 		Assertions.assertEquals(a / b, Pcalc.div(a, b), EPS);
 	}
 
+	@DisplayName("Test division of strings ")
+	@ParameterizedTest(name = "\"{0}\" by \"{1}\"")
+	@MethodSource("getDivisionOperands")
+	void testStringDivision(double a, double b)
+	{
+		Assertions.assertEquals(
+			a / b, Pcalc.div(Double.toString(a), Double.toString(b)), EPS
+		);
+	}
+
+	@DisplayName("Test a format exception on division of strings ")
+	@ParameterizedTest(name = "\"{0}\" by \"{1}\"")
+	@CsvSource({"a,1.1", "2,a"})
+	void testStringDivisionFormatException(String a, String b)
+	{
+		Assertions.assertThrows(
+			NumberFormatException.class, () -> Pcalc.div(a, b)
+		);
+	}
+
 	static Stream<Arguments> getSquareOperands() {
 		return Stream.of(
 			Arguments.of(2.2),
@@ -85,5 +166,25 @@ public class PcalcTest
 	void testSquare(double a)
 	{
 		Assertions.assertEquals(a * a, Pcalc.pow2(a), EPS);
+	}
+
+	@DisplayName("Test power 2 of a string ")
+	@ParameterizedTest(name = "\"{0}\"")
+	@MethodSource("getSquareOperands")
+	void testStringSquare(double a)
+	{
+		Assertions.assertEquals(
+			a * a, Pcalc.pow2(Double.toString(a)), EPS
+		);
+	}
+
+	@DisplayName("Test a format exception on power 2 of a string ")
+	@ParameterizedTest(name = "\"{0}\"")
+	@CsvSource({"a"})
+	void testStringSquareFormatException(String a)
+	{
+		Assertions.assertThrows(
+			NumberFormatException.class, () -> Pcalc.pow2(a)
+		);
 	}
 }
